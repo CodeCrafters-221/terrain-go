0b4f6c8c530ad175d33f2208ca92db5d80b406f4
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2026-01-27 22:31:38 +0000

    ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,16 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Si vous utilisez le router
-import LoginModal from "./LoginModal";
-import SignupModal from "./SignupModal";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+import { supabase } from "../services/supabaseClient";
 
 export default function Header() {
+  const { user } = useAuth();
   const navigate = useNavigate();
 
-  // États pour gérer la visibilité des modales
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  const [isSignupOpen, setIsSignupOpen] = useState(false);
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
 
-  // --- Fonctions de gestion ---
-
-  const openLogin = () => {
-    setIsSignupOpen(false); // Ferme l'inscription si ouverte
-    setIsLoginOpen(true); // Ouvre la connexion
-  };
-
-  const openSignup = () => {
-    setIsLoginOpen(false); // Ferme la connexion si ouverte
-    setIsSignupOpen(true); // Ouvre l'inscription
-  };
-
-  const closeAll = () => {
-    setIsLoginOpen(false);
-    setIsSignupOpen(false);
-  };
-
-  const handleSuccess = () => {
-    closeAll();
-    navigate("/dashboard"); // Redirection après succès
-  };
+    navigate("/");
+  }
 
   return (
     <>
@@ -65,31 +45,23 @@ export default function Header() {
             Pour les propriétaires
           </a>
 
-          <button
-            onClick={openLogin}
-            className="bg-primary rounded-full font-montserrat font-semibold text-text text-lg px-4 py-2 cursor-pointer hover:bg-primary/90 transition-colors"
-          >
-            Se connecter
-          </button>
+          {
+            user ?
+              <>
+                <Link to="/profile">Profil</Link>
+                <button onClick={handleLogout}>Logout</button>
+              </>
+              :
+              <Link to="/login"
+                onClick={() => { }}
+                className="bg-primary rounded-full font-montserrat font-semibold text-text text-lg px-4 py-2 cursor-pointer hover:bg-primary/90 transition-colors"
+              >
+                Se connecter
+              </Link>
+          }
+
         </div>
       </div>
-
-      {/* --- Intégration des Modales --- */}
-
-      {/* Modal de Connexion */}
-      <LoginModal
-        isOpen={isLoginOpen}
-        onClose={() => setIsLoginOpen(false)}
-        onSuccess={handleSuccess}
-        onSwitchToSignup={openSignup} // Permet de passer à l'inscription depuis le login
-      />
-
-      {/* Modal d'Inscription */}
-      <SignupModal
-        isOpen={isSignupOpen}
-        onClose={() => setIsSignupOpen(false)}
-        onSuccess={handleSuccess}
-      />
     </>
   );
 }
