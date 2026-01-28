eabef780a3156b952e62649b87920784320a7d08
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-01-28 00:56:28 +0000

    feat: add `AppLayout` component to define the main application structure with a conditionally rendered header.

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
 export default function AppLayout() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/search", "/profile"];
+  const shouldShowHeader = !hideHeaderRoutes.includes(location.pathname);
+
   return (
     <>
-      <Header />
+      {shouldShowHeader && <Header />}
       <main>
         <Outlet />
       </main>
@@ -12,3 +18,4 @@ export default function AppLayout() {
     </>
   );
 }
+
