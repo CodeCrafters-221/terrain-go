9003bc10e150588ed8462fe7c034a7644e56bc29
boy-aad <diallo.abdoulaziz0202@gmail.com>
2026-02-09 00:34:49 +0000

    ...

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,77 +2,79 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../services/supabaseClient";
 
 const AuthContext = createContext(null);
-
 export default function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    const [profile, setProfile] = useState(null);
-    const [terrains, setTerrains] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+  const [terrains, setTerrains] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const initSession = async () => {
-            const { data } = await supabase.auth.getSession();
-            setUser(data.session?.user ?? null);
-            setLoading(false);
-        };
+  useEffect(() => {
+    const initSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setUser(data.session?.user ?? null);
+      setLoading(false);
+    };
 
-        initSession();
+    initSession();
 
-        const { data: { subscription } } =
-            supabase.auth.onAuthStateChange((_event, session) => {
-                setUser(session?.user ?? null);
-            })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
 
-        return () => subscription.unsubscribe();
-    }, []);
+    return () => subscription.unsubscribe();
+  }, []);
 
-    useEffect(() => {
-        const fetchProfiles = async () => {
-            if (!user) {
-                setProfile(null);
-                return;
-            }
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      if (!user) {
+        setProfile(null);
+        return;
+      }
 
-            const { data, error } = await supabase.from("profiles")
-                .select("*")
-                .eq("id", user.id)
-                .maybeSingle();
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", user.id)
+        .maybeSingle();
 
-            if (!error) {
-                setProfile(data);
-            } else {
-                console.error("Erreur Profil: ", error.message);
-                setProfile(null);
-            }
+      if (!error) {
+        setProfile(data);
+      } else {
+        console.error("Erreur Profil: ", error.message);
+        setProfile(null);
+      }
+    };
+
+    fetchProfiles();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchTerrains = async () => {
+      if (profile.role === "owner") {
+        const { data, error } = await supabase
+          .from("fields")
+          .select("*")
+          .eq("proprietaire_id", profile.id);
+
+        if (!error) {
+          setTerrains(data);
+        } else {
+          console.error("Erreur récuperation terrain: ", error.message);
+          setTerrains(null);
         }
+      }
+    };
 
-        fetchProfiles();
-    }, [user]);
+    fetchTerrains();
+  }, [profile]);
 
-    useEffect(() => {
-        const fetchTerrains = async () => {
-            if(profile.role === "owner") {
-                const { data, error } = await supabase.from("fields")
-                .select("*")
-                .eq("proprietaire_id", profile.id);
-
-                if(!error) {
-                    setTerrains(data);
-                } else {
-                    console.error("Erreur récuperation terrain: ", error.message);
-                    setTerrains(null);
-                }
-            }
-        }
-
-        fetchTerrains();
-    }, [profile]);
-
-    return (
-        <AuthContext.Provider value={{ user, loading, profile, terrains }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, loading, profile, terrains }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export const useAuth = () => useContext(AuthContext);
