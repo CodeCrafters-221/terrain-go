91cb5fe330580b7638c05c6815cfb689a6dc8ef3
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2026-03-25 16:25:14 +0000

    ..

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -9,16 +9,10 @@ export default function AppLayout() {
   const shouldShowHeader = !hideHeaderRoutes.includes(location.pathname);
 
   return (
-<<<<<<< HEAD
-    <>
-      <Header />
-      <main>
-=======
     <div className="min-h-screen bg-background-dark overflow-x-hidden flex flex-col">
       {shouldShowHeader && <Header />}
 
       <main className={`flex-1 ${shouldShowHeader ? "pt-20 md:pt-24" : ""}`}>
->>>>>>> c1eb517d823af4dfa0650358f8eaa659c67727b4
         <Outlet />
       </main>
 
